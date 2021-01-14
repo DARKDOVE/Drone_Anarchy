88be121a56b7b68d67d3350dafe98410070d4427
Bluemoon <bluemagnificent@gmail.com>
2021-01-14 10:45:33 +0100

    License Upadate

--- Source/DroneAnarchy.h
+++ Source/DroneAnarchy.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2014 - 2020 Drone Anarchy.
+// Copyright (c) 2014 - 2021 Drone Anarchy.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
